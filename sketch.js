865b86cbe43c4bdef54cedd68215bc77b972e8d7
adeevdusty <devaiahadeev@gmail.com>
2020-08-09 16:08:43 +0530

    new platform added

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
-
+    platform1 = new Ground(350, 305, 300, 170);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -49,7 +49,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+    
     box3.display();
     box4.display();
     pig3.display();
@@ -61,4 +61,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    platform1.display();
 }
